536894dba02f2ed6ec3751e1414d5a9b3d6f09f5
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2022-11-30 11:51:22 -0300

    add: calc body fat percentage for female

--- scripts/script.js
+++ scripts/script.js
@@ -20,9 +20,12 @@ genderSelect.addEventListener('input', () => {
 btnCalculateForm.addEventListener('click', event => {
     event.preventDefault()
 
-    let calc = (495 / ((1.0324 - 0.19077 * Math.log10(waistInput.value - neckInput.value)) + (0.15456 * Math.log10(heightInput.value)))) - 450
+    let calcBFPMale = (495 / ((1.0324 - 0.19077 * Math.log10(waistInput.value - neckInput.value)) + (0.15456 * Math.log10(heightInput.value)))) - 450
+    let calcBFPFemale = (495 / ((1.29579 - 0.35004 * Math.log10(waistInput.value + hipInput.value - neckInput.value)) + (0.22100 * Math.log10(heightInput.value)))) - 450
 
     if (genderSelect.value == 'male') {
-        console.log(calc.toFixed(1))
+        console.log(calcBFPMale.toFixed(1))
+    } else {
+        console.log(calcBFPFemale.toFixed(1))
     }
 })
